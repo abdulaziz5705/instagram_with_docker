14f958376f30dd42071ba75a415be3b3a9844b6c
Abdulaziz Shukurov <abdulazizshukurov5705>
2024-11-29 11:41:57 +0500

    all

--- Core/settings.py
+++ Core/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-)!bcat$_^u0e3!^o+laeo)oqd8514710)!)_ai(ik(=87oi-n!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
